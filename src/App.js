40a5014b4dbc79a2f36340c5dbbf30e23cc43858
gzhang <gzhang@empirix.com>
2020-08-20 23:17:58 -0400

    Integrate Google Analytics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import ResumeHeader from './components/ResumeHeader';
 import ResumeContent from './components/resume-contents/ResumeContent';
 import Grid from '@material-ui/core/Grid';
+import ReactGA from 'react-ga';
 
 function App() {
+  // componentDidMount
+  useEffect(() => {
+    ReactGA.initialize('UA-175988035-1');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
   return (
       <Grid container justify="center">
           <Grid container item xs={4}>
