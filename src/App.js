812e223eb07a28788dd87d00386783fd77f7acc2
Guilin Zhang <GuilinDev@gmail.com>
2020-08-20 22:41:28 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import ResumeHeader from './components/ResumeHeader';
 import ResumeContent from './components/resume-contents/ResumeContent';
