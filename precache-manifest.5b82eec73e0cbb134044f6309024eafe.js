8c6036f49f0df4214b58100095a8bf9489286f02
GuilinDev <GuilinDev@gmail.com>
2020-08-21 02:42:37 +0000

    Deploying to gh-pages from  @ 812e223eb07a28788dd87d00386783fd77f7acc2 🚀

---
+++ precache-manifest.5b82eec73e0cbb134044f6309024eafe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a231520c7aa0be584e348bf58993e7b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da9c7a0e5453a0bbef14",
+    "url": "/static/css/main.b52b6472.chunk.css"
+  },
+  {
+    "revision": "a963715952b94adb4699",
+    "url": "/static/js/2.05116abc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.05116abc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da9c7a0e5453a0bbef14",
+    "url": "/static/js/main.bfb442d9.chunk.js"
+  },
+  {
+    "revision": "43a62b9c014d0ba94a80",
+    "url": "/static/js/runtime-main.60a07d1e.js"
+  }
+]);
